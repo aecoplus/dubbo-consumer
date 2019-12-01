b43a68fb491e39d8e5f0528deb5ad5eaa1595d00
aecoplus <jxl0126@163.com>
2019-12-01 10:33:02 +0800

    init

---
+++ src/test/java/com/aecoplus/dubboconsumer/DubboConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aecoplus.dubboconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
